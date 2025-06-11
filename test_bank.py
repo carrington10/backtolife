df58e301dd606a48e0becd049aac6636f713885f
nonne <carringtonhate@gmail.com>
2025-06-10 19:35:34 -0500

    adding in test case for bank

--- test_bank.py
+++ test_bank.py
@@ -5,7 +5,7 @@ class TestBank(unittest.TestCase):
     def test_list_account_info(self):
         account = bank("tom","09/12/2005","Seattle Washington","355678")
         expected = "This is your account info \nName:tom \nDob:09/12/2005 \nAccount Number:355678"
-        self.asseertEqual(account.listaccountinfo(),expected)
+        self.assertEqual(account.listaccountinfo(),expected)
 
 if __name__ == '__main__':
     unittest.main()
